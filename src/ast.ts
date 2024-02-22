c670368cf58243112e2ee0368a1caa8d38112339
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-02-22 21:59:35 +0100

    Collapsing tree

--- src/ast.ts
+++ src/ast.ts
@@ -120,7 +120,7 @@ function sya(tokens: Token[]): string[] {
     return queue
 }
 
-//console.log(sya(tokens).join(""))
+let rpn = sya(tokens)
 
 function rpevaluator(rps: string[]): number { 
     let stack: string[] = []
@@ -146,4 +146,89 @@ function rpevaluator(rps: string[]): number {
    return parseInt(operations[stack[0] as Operation](stack[1] as NumericLiteral, stack[2] as NumericLiteral) as string)
 }
 
-console.log(rpevaluator(sya(tokens)))
+let rpn_tree = rpn.reverse()
+
+class aNode {
+    //type: string
+    value: string
+    left: aNode | undefined
+    right: aNode | undefined
+
+    constructor(/*type: string, */value: string) {
+        //this.type = type
+        this.value = value
+        this.left = undefined
+        this.right = undefined
+    }
+}
+
+class AST {
+    root: aNode | undefined
+
+    constructor() {
+        this.root = undefined
+    }
+
+    Add(rpn: string[]): aNode {
+        let length = rpn.length
+        let current = this.root
+        let stack: aNode[] = []
+        
+        for (let i = 0; i < length; i++) {
+            let node = new aNode(rpn[i])
+
+            if (isBinaryOperation(rpn[i])) {
+                if (!this.root) {
+                    this.root = node
+                    current = this.root
+                    stack.push(current)
+                } else {
+                    if (isNumericalLiteral(current!.value)) {
+                        current = stack.pop()
+                    }
+                    if (!current!.right) {
+                        current!.right = node
+                        current = current!.right
+                    }
+                    else if (!current!.left) {
+                        current!.left = node
+                        current = current!.left
+                    }
+                    stack.push(current!)
+                }
+            } else {
+                if (isNumericalLiteral(current!.value)) {
+                    current = stack.pop()
+                } 
+                if (!current!.right) {
+                    current!.right = node
+                    current = current!.right
+                }
+                else if (!current!.left) {
+                    current!.left = node
+                    current = current!.left
+                }
+            }
+        }
+        return this.root!
+    }
+    Collapse(root: aNode): NumericLiteral {
+        let current: aNode = root
+        if (isNumericalLiteral(current.right!.value) && isNumericalLiteral(current.left!.value)) {
+            return operations[current.value as Operation](current.left!.value as NumericLiteral, current.right!.value as NumericLiteral)
+        }
+        else {
+            if (isBinaryOperation(current.left!.value) && isNumericalLiteral(current.right!.value))
+                current.value = operations[current.value as Operation](this.Collapse(current.left!) as NumericLiteral, current.right!.value as NumericLiteral)
+            else if (isBinaryOperation(current.right!.value) && isNumericalLiteral(current.left!.value))
+                current.value = operations[current.value as Operation](current.left!.value as NumericLiteral, this.Collapse(current.right!) as NumericLiteral)
+            else if (isBinaryOperation(current.left!.value) && isBinaryOperation(current.right!.value)) 
+                current.value = operations[current.value as Operation](this.Collapse(current.left!) as NumericLiteral, this.Collapse(current.right!) as NumericLiteral)
+        }
+        return current.value as NumericLiteral
+    }
+}
+
+let ast = new AST()
+ast.Add(rpn_tree)
+console.log(ast.Collapse(ast.root!))
